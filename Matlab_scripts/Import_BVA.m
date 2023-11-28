032ac7b0e30ad63af87d0da0ef569036a0169ad5
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-11-28 21:47:09 +0100

    Update Import_BVA.m

--- Matlab_scripts/Import_BVA.m
+++ Matlab_scripts/Import_BVA.m
@@ -18,4 +18,55 @@ dat.dataset         = ft_read_data(EEG_File.eeg,'header', EEG_File.vhdr);
 dat.label           = EEG_File.vhdr.label;
 
 
+findTrial           = [];
+findTime            = [];
+
+for n = 1:length(EEG_File.vmrk)
+    t = EEG_File.vmrk(n).type;                                            % Examine the marker file to find the length of each trial
+    if strcmp(t,'New Segment')                                            % and the start/end time.
+      findTrial = [findTrial;EEG_File.vmrk(n).sample]; 
+    end
+    if strcmp(t,'Time 0')
+      findTime = [findTime;EEG_File.vmrk(n).sample]; 
+    end    
+end
+
+if length(findTrial) == 1
+   trialLength = size(dat.dataset,2);                                      % Check if the data is 1 trial long (ERP), or greater than 1 trial long  
+   numTrial = 1;                                                           % (time-frequency analyses).
+else                                                                        
+   trialLength = findTrial(2) - findTrial(1);  
+   numTrial = size(dat.dataset,2)/trialLength; 
+end
+
+
+StartTime           = -(0 - 1)/EEG_File.vhdr.Fs;
+EndTime             = StartTime + (trialLength/EEG_File.vhdr.Fs); 
+
+ftTime              = [];
+ftTrial             = [];
+EEG_File.eegtime    = StartTime:(1/EEG_File.vhdr.Fs):(EndTime - (1/EEG_File.vhdr.Fs)); 
+
+
+for t = 1:numTrial
+    trl = dat.dataset(:,(1 + trialLength*(t-1)):trialLength*t);             % Match the time and trials with Fieldtrip's format
+    ftTrial{1,t} = trl;  
+    ftTime{1,t} = EEG_File.eegtime;
+end
+                                                                            % Create a data structure with all the relevant fields
+dat.time           = ftTime;
+dat.trial          = ftTrial;
+data               = ft_preprocessing([], dat);
+
+EEG_File.dat       = dat;
+EEG_File.data      = data;
+EEG_File.channels  = cell2table(EEG_File.vhdr.label);                       % Creates a list of all EEG Channel electrode names
+
+
+%Clean-UP
+clear EndTime findTime ftTime ftTrial n numTrial StartTime t trialLength trl dat data EEG_file findTrial
+
+%Save DATA
+
+
 
