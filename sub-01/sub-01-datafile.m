6cc18c2708eecff61a89da8bca7626756c3218e0
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-11-28 21:22:35 +0100

    Update sub-01-datafile.m

--- sub-01/sub-01-datafile.m
+++ sub-01/sub-01-datafile.m
@@ -5,10 +5,14 @@
 % | Author: Philipp Klocke,Moritz Löffler                | 
 % +------------------------------------------------------+
 
-%This script will serve as the main directory matlab file for sub-01
+% This script will serve as the main directory matlab file for sub-01
 
+% Chose the pathway for the SenseFOG-main folder after downloading
+subjectdata.generalpath     = uigetdir;
+subjectdata.filedir         = 'sub-01';
+subjectdata.subjectnr       = '01';
+subjectdata.subject_dir     = 'sub-01';
+path                        = append(subjectdata.generalpath, '/', subjectdata.filedir);
+cd(path)
 
-subjectdata.generalpath = ['/Users/philipp/Desktop/SenseFog_Data'];
-subjectdata.filedir = 'RawData/DBS01';                     %name of map raw data is saved
-subjectdata.subjectnr = '01';  
-subjectdata.subject_dir = 'DBS01';
+% Import the EEG File for 
