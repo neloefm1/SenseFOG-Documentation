94f58d823ab568d1e6041e336008589c73c3ce4a
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-11-28 21:26:13 +0100

    Update Import_JSON.m

--- Matlab_scripts/Import_JSON.m
+++ Matlab_scripts/Import_JSON.m
@@ -1,1 +1,9 @@
+%%=====  JSON File Import Script ========================================%
+% Original author(s): Philipp Klocke, Moritz Loeffler
+
+%This script will import the original raw data of specified LFPs
+%and save the data to a pre-determined filepath. We will start by importing
+%the files, visualising them for clarity purposes, and resampling them 
+%according to the EEG sampling rate. This step will necessitate the prior
+%execution of the EEG Import (BVAImport.m) script.
 
