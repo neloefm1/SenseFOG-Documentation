8ba58f3412394ee1ec86b1137324022a72c80c95
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-11-28 22:14:25 +0100

    Update Import_BVA.m

--- Matlab_scripts/Import_BVA.m
+++ Matlab_scripts/Import_BVA.m
@@ -2,13 +2,12 @@
 %Original author(s): Philipp Klocke, Moritz Loeffler
 
 %This script will import the original raw data of specified EEG data and save the data to a pre-determined filepath. 
-
-
 %==============================================================================
 close all
 
 EEG_File            = struct;                                            % Create a file structure
-[EEG_file,file]     = uigetfile('*.eeg');                                % Choose the desired EEG File
+[EEG_file,path]     = uigetfile('*.eeg');                                % Choose the desired EEG File
+cd(path)
 EEG_File.filename   = strrep(EEG_file,'.eeg','');                        % Creates the filename by cutting off the suffix (.eeg or .vmrk or .vhdr)
 EEG_File.eeg        = [EEG_File.filename,'.eeg'];                        % Read the data, header, and marker file.
 EEG_File.vhdr       = ft_read_header([EEG_File.filename,'.vhdr']);
@@ -16,7 +15,6 @@ EEG_File.vmrk       = ft_read_event([EEG_File.filename,'.vmrk']);
 EEG_File.fs_eeg     = EEG_File.vhdr.Fs;
 dat.dataset         = ft_read_data(EEG_File.eeg,'header', EEG_File.vhdr);
 dat.label           = EEG_File.vhdr.label;
-
 
 findTrial           = [];
 findTime            = [];
@@ -67,6 +65,6 @@ EEG_File.channels  = cell2table(EEG_File.vhdr.label);                       % Cr
 clear EndTime findTime ftTime ftTrial n numTrial StartTime t trialLength trl dat data EEG_file findTrial
 
 %Save DATA
+save([path EEG_File.filename '_raw.mat'], 'EEG_File', '-mat')       
 
-
-
+%% *********************** END OF SCRIPT ************************************************************************************************************************
