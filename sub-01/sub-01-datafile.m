db208d794f34ce5d8cd8e62e26bfde4473b1adf0
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-11-28 20:49:44 +0100

    Update sub-01-datafile.m

--- sub-01/sub-01-datafile.m
+++ sub-01/sub-01-datafile.m
@@ -1,1 +1,14 @@
+%% sub-01-datafile.m 
+% +------------------------------------------------------+
+% |  Subject: SUB-01                                     |
+% |                                                      |
+% | Author: Philipp Klocke,Moritz Löffler                | 
+% +------------------------------------------------------+
 
+%This script will serve as the main directory matlab file for sub-01
+
+
+subjectdata.generalpath = ['/Users/philipp/Desktop/SenseFog_Data'];
+subjectdata.filedir = 'RawData/DBS01';                     %name of map raw data is saved
+subjectdata.subjectnr = '01';  
+subjectdata.subject_dir = 'DBS01';
