b09b450110e309feea7bfb21726e51764d14a948
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-11-28 22:42:19 +0100

    Update Import_JSON.m

--- Matlab_scripts/Import_JSON.m
+++ Matlab_scripts/Import_JSON.m
@@ -1,9 +1,39 @@
-%%=====  JSON File Import Script ========================================%
+%%=====  Import_JSON.m ========================================%
 % Original author(s): Philipp Klocke, Moritz Loeffler
 
 %This script will import the original raw data of specified LFPs
 %and save the data to a pre-determined filepath. We will start by importing
-%the files, visualising them for clarity purposes, and resampling them 
-%according to the EEG sampling rate. This step will necessitate the prior
-%execution of the EEG Import (BVAImport.m) script.
+%the files, and resample them according to the EEG sampling rate.
+%==============================================================================
 
+close all
+
+LFP                       = struct;
+[JSON_file, path]         = uigetfile('*.json')                                                   % Defines the file and the file-path for our JSON file
+cd(path)
+JSON_filename             = strrep(JSON_file,'.json','');                                         % Creates the filename by cutting off the suffix .json
+LFP.rawfile               = jsondecode(fileread(JSON_file));
+
+%Assign the correct side to the LFP dataset
+if contains(LFP.rawfile.BrainSenseTimeDomain(1).Channel, "RIGHT") == 1; 
+    LFP.raw_lfp_right     = LFP.rawfile.BrainSenseTimeDomain(1).TimeDomainData
+    LFP.raw_lfp_left      = LFP.rawfile.BrainSenseTimeDomain(2).TimeDomainData
+elseif contains(LFP.rawfile.BrainSenseTimeDomain(1).Channel, "Left") == 1;
+    LFP.raw_lfp_left      = LFP.rawfile.BrainSenseTimeDomain(1).TimeDomainData
+    LFP.raw_lfp_right     = LFP.rawfile.BrainSenseTimeDomain(2).TimeDomainData
+end
+
+LFP.fs_lfp            = LFP.rawfile.BrainSenseTimeDomain(1).SampleRateInHz;                       % Sampling Rate in Hz
+LFP.lfptime           = (1/LFP.fs_lfp):(1/LFP.fs_lfp):(length(LFP.raw_lfp_right)/LFP.fs_lfp);     % Creating a time vector for our LFP Data 
+
+%Resampling LFP signal to EEG sampling rate
+LFP.interp_LFP_right = resample(LFP.raw_lfp_right,1000, LFP.fs_lfp)';                             % EEG sampling rate: 1000 Hz
+LFP.interp_LFP_left = resample(LFP.raw_lfp_left,1000, LFP.fs_lfp)';
+
+%Save DATA
+save([path JSON_filename '_raw.mat'], 'LFP', '-mat') 
+
+%Clean-UP
+clear JSON_filename
+
+%% *********************** END OF SCRIPT ************************************************************************************************************************
