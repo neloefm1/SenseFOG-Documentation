6b82bbe1a8aa33c03117a4278488fe32dfada767
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-12-01 21:40:36 +0100

    Update sub-15-datafile.m

--- sub-15/sub-15-datafile.m
+++ sub-15/sub-15-datafile.m
@@ -2,7 +2,7 @@
 % +------------------------------------------------------+
 % |  Subject: SUB-15                                     |
 % |                                                      |
-% | Author: Philipp Klocke,Moritz Löffler                | 
+% | Author: Philipp Klocke,Moritz Loeffler               | 
 % +------------------------------------------------------+
 
 % This script will serve as the main directory matlab file for sub-15
