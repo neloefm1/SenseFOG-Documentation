8a62398b5ca1f288ba08a63b1348faacbe4f97b8
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-12-01 20:24:59 +0100

    Create sub-05-datafile.m

---
+++ sub-05/sub-05-datafile.m
@@ -0,0 +1,158 @@
+%% sub-05-datafile.m ====================================================
+% +------------------------------------------------------+
+% |  Subject: SUB-05                                     |
+% |                                                      |
+% | Author: Philipp Klocke,Moritz Löffler                | 
+% +------------------------------------------------------+
+
+% This script will serve as the main directory matlab file for sub-01
+
+% Chose the pathway for the SenseFOG-main folder after downloading
+subjectdata.generalpath     = uigetdir;                                                                                         % Target file should be the SenseFOG-main file after download
+subjectdata.filedir         = 'sub-01';
+subjectdata.subjectnr       = '05';
+subjectdata.subject_dir     = 'sub-05';
+path                        = append(subjectdata.generalpath, '/', subjectdata.filedir);
+cd(path)
+
+
+% SIT - Manual Input for EEG and LFP Datasets
+subjectdata.signalpoint.Sit = [];
+subjectdata.signalpoint.Sit.EEG_signal          = [];                                                                           % Sample where DBS Stimulation stops showing a clear downward spike, used for later alignsignals_new.m
+subjectdata.signalpoint.Sit.LFP_signal          = [];                                                                           % Sample where DBS Stimulation stops showing a clear upward spike, used for later alignsignals_new.m
+if subjectdata.signalpoint.Sit.LFP_signal > subjectdata.signalpoint.Sit.EEG_signal;                                             % If clause to see if LFP signal is longer than EEG signal 
+   subjectdata.signalpoint.Sit.delay = subjectdata.signalpoint.Sit.LFP_signal - subjectdata.signalpoint.Sit.EEG_signal;         % Find time-delay between both EEG and LFP
+else subjectdata.signalpoint.Sit.EEG_signal > subjectdata.signalpoint.Sit.LFP_signal;                                           % If clause to see if EEG signal is longer than LFP signal
+     subjectdata.signalpoint.Sit.delay = subjectdata.signalpoint.Sit.EEG_signal - subjectdata.signalpoint.Sit.LFP_signal;       % Find time-delay between both EEG and LFP
+end 
+
+
+% STAND - Manual Input for EEG and LFP Datasets
+subjectdata.signalpoint.Stand = [];
+subjectdata.signalpoint.Stand.EEG_signal        = 7296;                                                                         % Sample where DBS Stimulation stops showing a clear downward spike, used for later alignsignals_new.m
+subjectdata.signalpoint.Stand.LFP_signal        = 15987;                                                                        % Sample where DBS Stimulation stops showing a clear upward spike, used for later alignsignals_new.m
+if subjectdata.signalpoint.Stand.LFP_signal > subjectdata.signalpoint.Stand.EEG_signal;                                         % If clause to see if LFP signal is longer than EEG signal 
+   subjectdata.signalpoint.Stand.delay = subjectdata.signalpoint.Stand.LFP_signal - subjectdata.signalpoint.Stand.EEG_signal;   % Find time-delay between both EEG and LFP
+else subjectdata.signalpoint.Stand.EEG_signal > subjectdata.signalpoint.Stand.LFP_signal;                                       % If clause to see if EEG signal is longer than LFP signal
+     subjectdata.signalpoint.Stand.delay = subjectdata.signalpoint.Stand.EEG_signal - subjectdata.signalpoint.Stand.LFP_signal; % Find time-delay between both EEG and LFP
+end 
+
+% WALK - Manual Input for EEG and LFP Datasets
+subjectdata.signalpoint.Walk = [];
+subjectdata.signalpoint.Walk.EEG_signal         = [];                                                                           % Sample where DBS Stimulation stops showing a clear downward spike, used for later alignsignals_new.m
+subjectdata.signalpoint.Walk.LFP_signal         = [];                                                                           % Sample where DBS Stimulation stops showing a clear upward spike, used for later alignsignals_new.m
+if subjectdata.signalpoint.Walk.LFP_signal > subjectdata.signalpoint.Walk.EEG_signal;                                           % If clause to see if LFP signal is longer than EEG signal 
+   subjectdata.signalpoint.Walk.delay = subjectdata.signalpoint.Walk.LFP_signal - subjectdata.signalpoint.Walk.EEG_signal;      % Find time-delay between both EEG and LFP
+else subjectdata.signalpoint.Walk.EEG_signal > subjectdata.signalpoint.Walk.LFP_signal;                                         % If clause to see if EEG signal is longer than LFP signal
+     subjectdata.signalpoint.Walk.delay = subjectdata.signalpoint.Walk.EEG_signal - subjectdata.signalpoint.Walk.LFP_signal;    % Find time-delay between both EEG and LFP
+end
+
+% WALK WITH STOPS - Manual Input for EEG and LFP Datasets
+subjectdata.signalpoint.WalkWS = [];
+subjectdata.signalpoint.WalkWS.EEG_signal       = 3883;                                                                         % Sample where DBS Stimulation stops showing a clear downward spike, used for later alignsignals_new.m
+subjectdata.signalpoint.WalkWS.LFP_signal       = 8885;                                                                         % Sample where DBS Stimulation stops showing a clear upward spike, used for later alignsignals_new.m
+if subjectdata.signalpoint.WalkWS.LFP_signal > subjectdata.signalpoint.WalkWS.EEG_signal;                                       % If clause to see if LFP signal is longer than EEG signal 
+   subjectdata.signalpoint.WalkWS.delay = subjectdata.signalpoint.WalkWS.LFP_signal - subjectdata.signalpoint.WalkWS.EEG_signal;% Find time-delay between both EEG and LFP
+else subjectdata.signalpoint.WalkWS.EEG_signal > subjectdata.signalpoint.WalkWS.LFP_signal;                                     % If clause to see if EEG signal is longer than LFP signal
+     subjectdata.signalpoint.WalkWS.delay = subjectdata.signalpoint.WalkWS.EEG_signal - subjectdata.signalpoint.WalkWS.LFP_signal;% Find time-delay between both EEG and LFP
+end 
+
+
+%=== FILTERED GAIT EVENTS FOR WALK ONLY ===
+% Manual input for filtered gait events based on exact timings of heelstrikes (not shown here)
+subjectdata.events_filt.Walk = struct;
+
+%Walk 1
+subjectdata.events_filt.Walk(1).start = 2.84400000000000;
+subjectdata.events_filt.Walk(1).task = 'Walk';
+subjectdata.events_filt.Walk(1).end = 25.0330000000000;
+
+%Walk 2
+subjectdata.events_filt.Walk(2).start = 37.1970000000000;
+subjectdata.events_filt.Walk(2).task = 'Walk';
+subjectdata.events_filt.Walk(2).end = 59.1930000000000;
+
+%Walk 3
+subjectdata.events_filt.Walk(3).start = 73.3430000000000;
+subjectdata.events_filt.Walk(3).task = 'Walk';
+subjectdata.events_filt.Walk(3).end = 93.3200000000000;
+
+%Turn 1
+subjectdata.events_filt.Walk(4).start =  95.0710000000000;
+subjectdata.events_filt.Walk(4).task = 'Turn';
+subjectdata.events_filt.Walk(4).end = 101.675000000000;
+
+%Walk 4
+subjectdata.events_filt.Walk(5).start = 103.216000000000;
+subjectdata.events_filt.Walk(5).task = 'Walk';
+subjectdata.events_filt.Walk(5).end = 128.467000000000;
+
+
+%=== FILTERED GAIT EVENTS FOR WALK WITH STOPS ONLY ===
+%Manual Input for Filtered Gait Events based on the exact timings of heelstrike events (not shown here)
+subjectdata.events_filt.WalkWS = struct;
+
+%Walk 1
+subjectdata.events_filt.WalkWS(1).task = 'Walk'; 
+subjectdata.events_filt.WalkWS(1).start = 35.0190000000000;
+subjectdata.events_filt.WalkWS(1).end = 37.9520000000000;
+
+%Selected stop
+subjectdata.events_filt.WalkWS(2).task = 'Selected_stop'; 
+subjectdata.events_filt.WalkWS(2).start = 39.1950;
+subjectdata.events_filt.WalkWS(2).end = 40.699;
+
+%Walk 2
+subjectdata.events_filt.WalkWS(3).task = 'Walk'; 
+subjectdata.events_filt.WalkWS(3).start = 41.2510;
+subjectdata.events_filt.WalkWS(3).end = 45.4910;
+
+%Walk 3
+subjectdata.events_filt.WalkWS(4).task = 'Walk'; 
+subjectdata.events_filt.WalkWS(4).start = 49.1540;
+subjectdata.events_filt.WalkWS(4).end = 51.9930;
+
+%Turn, omit after discussion
+%subjectdata.events_filt.WalkWS(5).task = 'Turn'; 
+%subjectdata.events_filt.WalkWS(5).start = 53.4880;
+%subjectdata.events_filt.WalkWS(5).end = 64.3750;
+
+%Walk 4
+subjectdata.events_filt.WalkWS(6).task = 'Walk'; 
+subjectdata.events_filt.WalkWS(6).start = 65.9620;
+subjectdata.events_filt.WalkWS(6).end = 77.8510;
+
+%Selected stop
+subjectdata.events_filt.WalkWS(7).task = 'Selected_stop'; 
+subjectdata.events_filt.WalkWS(7).start = 80.08;
+subjectdata.events_filt.WalkWS(7).end = 82.277;
+
+%Walk 5
+subjectdata.events_filt.WalkWS(8).task = 'Walk'; 
+subjectdata.events_filt.WalkWS(8).start = 82.8730;
+subjectdata.events_filt.WalkWS(8).end = 85.6060;
+
+%Selected stop
+subjectdata.events_filt.WalkWS(9).task  = 'Selected_stop'; 
+subjectdata.events_filt.WalkWS(9).start = 91.315;
+subjectdata.events_filt.WalkWS(9).end   = 92.606;
+
+%Walk 6
+subjectdata.events_filt.WalkWS(10).task = 'Walk'; 
+subjectdata.events_filt.WalkWS(10).start = 97.3860;
+subjectdata.events_filt.WalkWS(10).end = 98.7230;
+
+%Selected stop %After Review, Stop is meant to be exluded due to FI
+%subjectdata.events_filt.WalkWS(11).task = 'Selected_stop'; 
+%subjectdata.events_filt.WalkWS(11).start = 99.9860;
+%subjectdata.events_filt.WalkWS(11).end = ;
+
+%Walk 7
+subjectdata.events_filt.WalkWS(12).task = 'Walk'; 
+subjectdata.events_filt.WalkWS(12).start = 101.8600;
+subjectdata.events_filt.WalkWS(12).end = 106.2850;
+
+
+
+
+
