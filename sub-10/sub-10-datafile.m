6f9b796a5406a09f17de92b6f80be2f48e3381af
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-12-01 21:41:31 +0100

    Update sub-10-datafile.m

--- sub-10/sub-10-datafile.m
+++ sub-10/sub-10-datafile.m
@@ -2,7 +2,7 @@
 % +------------------------------------------------------+
 % |  Subject: SUB-10                                     |
 % |                                                      |
-% | Author: Philipp Klocke,Moritz Löffler                | 
+% | Author: Philipp Klocke,Moritz Loeffler               | 
 % +------------------------------------------------------+
 
 % This script will serve as the main directory matlab file for sub-01
