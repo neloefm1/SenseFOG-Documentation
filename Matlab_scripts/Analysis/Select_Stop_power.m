f2eef80332989c3043cf415bd6cad0da67ca46ae
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-12-03 17:23:26 +0100

    Update Select_Stop_power.m

--- Matlab_scripts/Analysis/Select_Stop_power.m
+++ Matlab_scripts/Analysis/Select_Stop_power.m
@@ -1,1 +1,84 @@
+%% =====  Select_Stop_power.m  ========================================%
 
+%Date: December 2023
+%Original author(s): Philipp Klocke, Moritz Loeffler
+
+%This script will first call all the timepoints for self-selected stops that
+%was specified and stored in the Sub_GrandActivity_Log.m file. We will
+%first compute the morlet wavelet spectra for the LFP time series and
+%segment the time-frequency spectra according to the prespecified time
+%points. In a second step, we will use the standing baseline and normalize
+%the power spectra yielding the mean %-change from standing. Third, we will
+%resample all epochs and bring them up to the same data length.
+%The current script will focus on data from the disease dominant STN.
+%===========================================================================%
+
+
+subjectdata.generalpath                 = uigetdir;                                                                 % Example: Call the SenseFOG-main file
+cd(subjectdata.generalpath)
+names                                   = cellstr(strsplit(sprintf('sub-%02d ',1:20)));                             % Create a list of sub-names
+
+for i = 1:20
+    if ~isfolder(names{i}) == 1                                                                                     % Check if general folder exists for sub-XX
+        fprintf(2," \n Missing Folder for %s ", names{i}); continue
+    elseif isfolder(names{i}) == 1
+        fprintf("\n Existing Folder for %s ", names{i})
+        full_filename = append(subjectdata.generalpath, "/", names{i},"/", names{i}, "-dataevents.mat");            % Search for the dataevents.mat file in each subfile
+        if isfile(full_filename); 
+            Files(i).File = load(full_filename);             
+            Files(i).name = sprintf('sub_%02d',i);
+        elseif ~isfile(full_filename); fprintf(2," \n Missing Dataevents File for %s ", names{i}); continue
+        end
+    end
+end
+clear i full_filename
+
+%Clear Files that have no content
+idx     = find(cellfun(@isempty,{Files.File})); Files(idx) = []; clear idx
+names   = {Files.name}';                                                                                             % Create a new names list
+
+Subjects = struct; 
+for i = 1:length(Files)
+    Subjects.(names{i}) = Files(i).File.LFP_Events;
+end
+
+
+%High Pass LFP Data ===================================================================================================
+%Highpass LFP Data (1 Hz cutoff, Butterworth filter, filter order6, passed
+%forward and backward) before applying continuous Morlet wavelet transformation
+[b,a] = butter(6,1/(1000/2),'high');
+
+task = {'Walk'; 'WalkWS'; 'WalkINT'; 'WalkINT_new'};
+for k = 1:length(names)
+    for m = 1:length(task)
+          if isfield(Subjects.(names{k}), task(m)) == 0; continue; end
+          Subjects.(names{k}).(task{m}).LFP_signal_R = filter(b,a, Subjects.(names{k}).(task{m}).LFP_signal_R);
+          Subjects.(names{k}).(task{m}).LFP_signal_L = filter(b,a, Subjects.(names{k}).(task{m}).LFP_signal_L); 
+    end
+end
+
+clear a b k m task Files
+
+
+%Apply Morlet Wavelet Transformation ===================================================================================
+%BE AWARE THIS WILL CONSUME A LOT OF SPACE AND TIME (full dataset of 12 subjects may take up to 20 minutes)
+task    = {'Walk'; 'WalkWS'; 'WalkINT'; 'WalkINT_new'};
+fs      = 1000; %Sampling Frequency
+for k = 1:length(names)
+    for m = 1:length(task)
+        if isfield(Subjects.(names{k}), task(m)) == 0; continue; end
+       
+        %Left FOOT ===
+        MFF = 4*pi/(6+sqrt(2+6^2));                                                                                 % Morlet-Fourier-Factor
+        R_cwtstruct          = cwtft({Subjects.(names{k}).(task{m}).LFP_signal_R,1/fs},'wavelet', 'morl','scales',1./([1:1:100]*MFF)); 
+        Subjects.(names{k}).(task{m}).R_wt                 = abs(R_cwtstruct.cfs);
+        Subjects.(names{k}).(task{m}).f                    = R_cwtstruct.frequencies;
+
+        %Right FOOT ===
+        MFF = 4*pi/(6+sqrt(2+6^2));                                                                                 % Morlet-Fourier-Factor
+        L_cwtstruct          = cwtft({Subjects.(names{k}).(task{m}).LFP_signal_L,1/fs},'wavelet', 'morl','scales',1./([1:1:100]*MFF)); 
+        Subjects.(names{k}).(task{m}).L_wt                 = abs(L_cwtstruct.cfs);
+        Subjects.(names{k}).(task{m}).f                    = L_cwtstruct.frequencies;
+    end
+end
+clear MFF R_cwtstruct L_cwtstruct k m task
