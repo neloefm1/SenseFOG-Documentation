70ff3897347970b1a37730e1f5eee88c97d6009f
PKlocke <152193415+PKlocke@users.noreply.github.com>
2023-12-12 11:33:24 +0100

    Update STN_EMG_Coherence_Pre_Stop.m

--- Matlab_scripts/Analysis/STN_EMG_Coherence_Pre_Stop.m
+++ Matlab_scripts/Analysis/STN_EMG_Coherence_Pre_Stop.m
@@ -1,1 +1,42 @@
-Text
+%% =====  STN_EMG_Coherence_Pre_Stop.m  ========================================%
+
+%Date: December 2023
+%Original author(s): Philipp Klocke, Moritz Loeffler
+
+%This script will first call all the timepoints for self-selected stop that
+%were specified and stored in the Sub_GrandActivity_Log.m file. Gait cycles occurring
+%before the stop will be identified. We will then insert both EMG and LFP 
+%timeseries into fieldtrip and compute the wavelet coherence between both signals. 
+%Second, we will use the standing coherence as our baseline and normalize
+%coherence spectra. Coherence spectra will be computed for the tibialis
+%anterior (TA) and gastrocnemius muscles (GA) yielding the subthalamo-muscular
+%coherence. Only the disease dominant STN will be focus of the current analysis. 
+%===========================================================================%
+
+subjectdata.generalpath                 = uigetdir;                                                                 % Example: Call the SenseFOG-main file
+cd(subjectdata.generalpath)
+names                                   = cellstr(strsplit(sprintf('sub-%02d ',1:20)));                             % Create a list of sub-names
+
+for i = 1:20
+    if ~isfolder(names{i}) == 1                                                                                     % Check if general folder exists for sub-XX
+        fprintf(2," \n Missing Folder for %s ", names{i}); continue
+    elseif isfolder(names{i}) == 1
+        fprintf("\n Existing Folder for %s ", names{i})
+        full_filename = append(subjectdata.generalpath, "/", names{i},"/", names{i}, "-dataevents.mat");            % Search for the dataevents.mat file in each subfile
+        if isfile(full_filename); 
+            Files(i).File = load(full_filename);             
+            Files(i).name = sprintf('sub_%02d',i);
+        elseif ~isfile(full_filename); fprintf(2," \n Missing Dataevents File for %s ", names{i}); continue
+        end
+    end
+end
+clear i full_filename
+
+%Clear Files that have no content
+idx     = find(cellfun(@isempty,{Files.File})); Files(idx) = []; clear idx
+names   = {Files.name}';                                                                                             % Create a new names list
+
+Subjects = struct; 
+for i = 1:length(Files)
+    Subjects.(names{i}) = Files(i).File.LFP_Events;
+end
