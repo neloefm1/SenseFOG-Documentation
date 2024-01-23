f137953f36f4cf021419de08179364729f857293
neloefm1 <105280627+neloefm1@users.noreply.github.com>
2024-01-23 20:42:24 +0100

    Update sub-09-datafile.m

--- Matlab_scripts/Subject_Files/sub-09-datafile.m
+++ Matlab_scripts/Subject_Files/sub-09-datafile.m
@@ -18,8 +18,8 @@ cd(path)
 
 % SIT - Manual Input for EEG and LFP Datasets
 subjectdata.signalpoint.Sit = [];
-subjectdata.signalpoint.Sit.EEG_signal          = 27238;                                                                        % Sample where DBS Stimulation stops showing a clear downward spike, used for later alignsignals_new.m
-subjectdata.signalpoint.Sit.LFP_signal          = 37218;                                                                        % Sample where DBS Stimulation stops showing a clear upward spike, used for later alignsignals_new.m
+subjectdata.signalpoint.Sit.EEG_signal          = 16724;                                                                        % Sample where DBS Stimulation stops showing a clear downward spike, used for later alignsignals_new.m
+subjectdata.signalpoint.Sit.LFP_signal          = 37128;                                                                        % Sample where DBS Stimulation stops showing a clear upward spike, used for later alignsignals_new.m
 if subjectdata.signalpoint.Sit.LFP_signal > subjectdata.signalpoint.Sit.EEG_signal;                                             % If clause to see if LFP signal is longer than EEG signal 
    subjectdata.signalpoint.Sit.delay = subjectdata.signalpoint.Sit.LFP_signal - subjectdata.signalpoint.Sit.EEG_signal;         % Find time-delay between both EEG and LFP
 else subjectdata.signalpoint.Sit.EEG_signal > subjectdata.signalpoint.Sit.LFP_signal;                                           % If clause to see if EEG signal is longer than LFP signal
