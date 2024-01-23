6b644ba59a391ecde765a582dadee97226d31712
PKlocke <152193415+PKlocke@users.noreply.github.com>
2024-01-23 19:33:03 +0100

    Update Data_Alignment.m

--- Matlab_scripts/Preprocessing/Data_Alignment.m
+++ Matlab_scripts/Preprocessing/Data_Alignment.m
@@ -17,18 +17,20 @@
 %Load EEG and LFP data and IMU data by specifying subject and SESSION of interest
 subjectdata.generalpath         = uigetdir;                                                         % Example: SenseFOG-main/sub-XX/ses-standing
 cd(subjectdata.generalpath)
-filename                        = extractAfter(subjectdata.generalpath,"-main/");                   % Create the specified filename
-filename                        = extractBefore(filename,"/ses-");                                  % Create the specified taskname
-taskname                        = extractAfter(subjectdata.generalpath,"/ses-");                    % Create the specified taskname
+filename                        = extractAfter(subjectdata.generalpath, "-main");                  % Create the specified filename
+filename                        = extractBefore(filename(2:end),"/ses-");                                  % Create the specified taskname
+taskname                        = extractAfter(subjectdata.generalpath,"ses-");                    % Create the specified taskname
 taskname                        = append("ses-", taskname);                                         % Create the specified taskname
-filepath                        = extractBefore(subjectdata.generalpath, "/ses");                   % Create the specified filepath
-fullname                        = append(subjectdata.generalpath, "/ieeg/",filename, "-", taskname, "_raw.mat"); 
+filepath                        = extractBefore(subjectdata.generalpath, "ses");                   % Create the specified filepath
+filepath                        = filepath(1:end-1); 
+fullname                        = append(subjectdata.generalpath, filesep, "ieeg", filesep,filename, "-", taskname, "_raw.mat"); 
+
 if isfile(fullname); load(fullname); end                                                            % LOAD JSON [LFP] FILE
-fullname                        = append(subjectdata.generalpath, "/eeg/",filename, "-", taskname, "_raw.mat"); 
+fullname                        = append(subjectdata.generalpath,filesep, "eeg", filesep,filename, "-", taskname, "_raw.mat"); 
 if isfile(fullname); load(fullname); end                                                            % LOAD BVA [EEG] FILE
-fullname                        = append(subjectdata.generalpath, "/motion/",filename, "-", taskname, "_raw.mat");
+fullname                        = append(subjectdata.generalpath,filesep, "motion", filesep, filename, "-", taskname, "_raw.mat");
 if isfile(fullname); load(fullname); end                                                            % LOAD HDF [IMU] FILE
-fullname                        = append(filepath, "/",filename, "_datafile.mat"); load(fullname);
+fullname                        = append(filepath, filesep, filename, "_datafile.mat"); load(fullname);
 
 subjectdata.filepath            = filepath;                                                         % Create the specified filepath
 subjectdata.fs_eeg              = 1000;                                                             % EEG sampling rate is 1000 Hz per default
